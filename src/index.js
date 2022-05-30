f898682b9749a7bbd17e9c1b493ed89baaf45fcf
Atorey <anatortosa15@gmail.com>
2022-05-30 13:41:48 +0200

    Server

--- src/index.js
+++ src/index.js
@@ -8,12 +8,12 @@ const walls = require(__dirname + '/routes/walls')
 const auth = require(__dirname + '/routes/auth')
 const users = require(__dirname + '/routes/users')
 
-/* mongoose.connect('mongodb+srv://atorey:Jk15wpoaq9c@cluster0.qxnl4.mongodb.net/test', {
+mongoose.connect('mongodb+srv://atorey:Jk15wpoaq9c@cluster0.qxnl4.mongodb.net/test', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-}) */
+})
 
-mongoose.connect('mongodb://localhost:27017/blocarc', { useNewUrlParser: true })
+/* mongoose.connect('mongodb://localhost:27017/blocarc', { useNewUrlParser: true }) */
 
 let app = express()
 
@@ -27,5 +27,5 @@ app.use('/users', users)
 app.use('/img/boulders', express.static('./public/img/boulders'))
 
 const PORT = 5000
-/* app.listen(process.env.PORT || PORT) */
-app.listen(PORT)
+app.listen(process.env.PORT || PORT)
+/* app.listen(PORT) */
