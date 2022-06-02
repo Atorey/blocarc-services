57e9436994e9367fc0b6f4dcbd40bbaba66628a0
Atorey <anatortosa15@gmail.com>
2022-06-02 19:03:47 +0200

    cors

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ mongoose.connect('mongodb+srv://atorey:Jk15wpoaq9c@cluster0.qxnl4.mongodb.net/te
  */
 let app = express()
 
-app.use(cors())
+app.use(cors({origin: true}))
 app.use(express.json({ limit: '50mb' }))
 app.use(express.static('public'))
 app.use('/boulders', boulders)
