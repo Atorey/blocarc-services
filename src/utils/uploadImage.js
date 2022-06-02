1164a27579b36be57eb4618def7b3e78bf0c5269
Atorey <anatortosa15@gmail.com>
2022-06-02 20:47:03 +0200

    Test compress imagesç

--- src/utils/uploadImage.js
+++ src/utils/uploadImage.js
@@ -1,11 +1,21 @@
-const fs = require('fs')
+/* const fs = require('fs') */
+const Buffer = require('buffer').Buffer
 const path = require('path')
 const sharp = require('sharp')
 
 const saveImage = async (dir, image) => {
-  await sharp(image).webp({ quality: 20 })
   const data = image.split(',')[1] || image
   const file = `${Date.now()}.webp`
+  const ref = path.join('public/img', dir, file)
+  const buffer = Buffer.from(data, 'base64')
+
+  await sharp(buffer).webp({ quality: 75 }).toFile(ref)
+  return `img/${dir}/${file}`
+}
+
+/* const saveImage = (dir, image) => {
+  const data = image.split(',')[1] || image
+  const file = `${Date.now()}.jpg`
   return new Promise((resolve, reject) => {
     const filePath = path.join('public/img', dir, file)
     fs.writeFile(filePath, data, { encoding: 'base64' }, err => {
@@ -14,7 +24,8 @@ const saveImage = async (dir, image) => {
       }
       resolve(`img/${dir}/${file}`)
     })
+    sharp(filePath).webp({ quality: 20 })
   })
-}
+} */
 
 module.exports = saveImage
